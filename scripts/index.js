4219b1cdc49973d0bcea95f19b93d486b9f6d679
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-20 18:09:38 +0200

    avancement du javascript

--- scripts/index.js
+++ scripts/index.js
@@ -1,13 +1,14 @@
 let totalScore = 0;
 let totalScoreHtml = document.getElementById("score");
 
+
 let totalMultiplicateur = 1;
-let totalMultiplicateurHtml = document.getElementById("multiplicateur");
+let totalMultiplicateurHtml = document.getElementById("multiplicateurText");
 
 let cookieClick = document.getElementById("cookie");
 
-let bonus1 = document.getElementById("bonus1");
-let bonus2 = document.getElementById("bonus2");
+let bonus1 = document.getElementById("bonus1"); //OK
+let bonus2 = document.getElementById("bonus2"); //OK
 let bonus3 = document.getElementById("bonus3");
 let bonus4 = document.getElementById("bonus4");
 
@@ -16,10 +17,20 @@ let bonus2Multiplicator = 3;
 let bonus3Multiplicator = 4;
 let bonus4Multiplicator = 5;
 
+let bonus1CostElement = document.getElementById("valeurCoutBonus1");
 let bonus1Cost = 10;
-let bonus2Cost = 100;
-let bonus3Cost = 1000;
-let bonus4Cost = 10000;
+let bonus1Increment = 5;
+bonus1CostElement.innerHTML = bonus1Cost;
+
+let bonus2CostElement = document.getElementById("valeurCoutBonus2");
+let bonus2Cost = 25;
+bonus2CostElement.innerHTML = bonus2Cost;
+
+// let bonus3Cost = 1000;
+// let bonus4Cost = 10000;
+console.log(bonus1Cost);
+let newBonus1cost = bonus1Cost + (bonus1Cost / 100 * 10);
+
 
 let bonus1Number = 0;
 let bonus2Number = 0;
@@ -36,43 +47,63 @@ function clickScore() {
 
 //auto clicker function
 function autoClicker() {
-  totalScore += autoClickNumber * totalMultiplicateur;
+  totalScore += autoClickNumber * 1;
+  totalScoreHtml.innerHTML = totalScore;
 }
 //window.setInterval(auto, 1000);
 
 cookieClick.addEventListener("click", clickScore);
 
 
+
+
 cookie.addEventListener("click", () => {
   totalScoreHtml.innerHTML = totalScore;
+
+  if (totalScore >= 10000){
+    bonus4.disabled = false;
+  } else if (totalScore >= 1000) {
+    bonus3.disabled = false;
+  } else if (totalScore >= bonus2Cost) {
+    bonus2.disabled = false;
+  } else if (totalScore >= bonus1Cost) {
+    bonus1.disabled = false;
+  } else {
+    bonus1.disabled = true;
+  }
 });
 
-/*
-LES ETAPES DU PROJET
-
-ETAPE 1 : Quand on clique sur "cookie" :
-  comportement score ==> totalScore = score++;
-
-ETAPE 2 : Quand score atteint :
-  - Palier 1 : coutBouton1 (default = 100 points)
-    comportement bouton bonus1 ==> si (totalScore >= 100) ==> attribut = enabled
-      Si je clique sur le bouton bonus1
-        comportement score ==> totalScore = totalScore - coutBouton1
-        comportement coutBouton1 = coutBouton1 + 10%
-        comportement bouton bonus1 = disabled
-        comportement multiplicateur = multiplicateur *2 ;
-        comportement click = score * multiplicateur ;
-          Concrêtement : chaque click devient +2;
-  - Palier 2 : coutBouton2 (default = 1000 points)
-    comportement bouton bonus 2 ==> si (totalScore >= 1000) ==> attribut = enabled
-      Si je clique sur le bouton bonus2
-        comportement score ==> totalScore = totalScore - coutBouton2
-        comportement coutBouton2 = coutBouton2 + 10%
-        comportement bouton bonus2 = disabled
-        comportement général = function autoclic = 1 click (nrépétition 0pardefault) / seconde
-          Concrètement, je clique 5 fois par seconde, mais l'ordinateur me compte un 6ème click ==> après 10 secondes, j'ai 50 clicks + 10 = 60 clicks
-  
+bonus1.addEventListener("click", () => {
+  totalMultiplicateur++;
+    totalMultiplicateurHtml.innerHTML = totalMultiplicateur;
+  totalScore = totalScore - bonus1Cost;
+    totalScoreHtml.innerHTML = totalScore;
+  bonus1Cost += bonus1Increment;
+    bonus1CostElement.innerHTML = bonus1Cost;
+    bonus1Increment +=10;
+  if (totalScore < bonus1Cost) {
+    bonus1.disabled = true;
+  } 
+    })
 
 
+bonus2.addEventListener("click", () =>{
+  autoClickNumber++;
+  setInterval(autoClicker, 1000);
+  totalScore = totalScore - bonus2Cost;
+  bonus2Cost += 25;
+    bonus2CostElement.innerHTML = bonus2Cost;
+  if (totalScore < bonus2Cost){
+    bonus2.disabled = true;
+  }
 
-*/
+})
+
+
+
+
+
+  // totalMultiplicateurHtml = document.getElementById("multiplicateur");
+  //  totalMultiplicateurHtml.innerHTML = totalMultiplicateur;
+  //  totalScore = calculerNewScore(bonus1Cost); 
+  // totalScoreHtml.innerHTML = totalScore;
