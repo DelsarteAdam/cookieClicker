565d79a0362375f05fbcf696e8304770d2b80d3a
Cédric Truyen <cedrictruyen@icloud.com>
2024-06-20 09:11:26 +0200

    Modification du script.js

--- scripts/index.js
+++ scripts/index.js
@@ -41,3 +41,38 @@ function autoClicker() {
 //window.setInterval(auto, 1000);
 
 cookieClick.addEventListener("click", clickScore);
+
+
+cookie.addEventListener("click", () => {
+  totalScoreHtml.innerHTML = totalScore;
+});
+
+/*
+LES ETAPES DU PROJET
+
+ETAPE 1 : Quand on clique sur "cookie" :
+  comportement score ==> totalScore = score++;
+
+ETAPE 2 : Quand score atteint :
+  - Palier 1 : coutBouton1 (default = 100 points)
+    comportement bouton bonus1 ==> si (totalScore >= 100) ==> attribut = enabled
+      Si je clique sur le bouton bonus1
+        comportement score ==> totalScore = totalScore - coutBouton1
+        comportement coutBouton1 = coutBouton1 + 10%
+        comportement bouton bonus1 = disabled
+        comportement multiplicateur = multiplicateur *2 ;
+        comportement click = score * multiplicateur ;
+          Concrêtement : chaque click devient +2;
+  - Palier 2 : coutBouton2 (default = 1000 points)
+    comportement bouton bonus 2 ==> si (totalScore >= 1000) ==> attribut = enabled
+      Si je clique sur le bouton bonus2
+        comportement score ==> totalScore = totalScore - coutBouton2
+        comportement coutBouton2 = coutBouton2 + 10%
+        comportement bouton bonus2 = disabled
+        comportement général = function autoclic = 1 click (nrépétition 0pardefault) / seconde
+          Concrètement, je clique 5 fois par seconde, mais l'ordinateur me compte un 6ème click ==> après 10 secondes, j'ai 50 clicks + 10 = 60 clicks
+  
+
+
+
+*/
