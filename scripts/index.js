1c23685348fb06f58c8ac60718f1925533c6fb93
Cedric Truyen <cedrictruyen@icloud.com>
2024-06-21 11:42:39 +0200

    Ajout du bouton 3

--- scripts/index.js
+++ scripts/index.js
@@ -1,6 +1,6 @@
 let totalScore = 0;
 let totalScoreHtml = document.getElementById("score");
-
+let countdown = 5;
 
 let totalMultiplicateur = 1;
 let totalMultiplicateurHtml = document.getElementById("multiplicateurText");
@@ -9,8 +9,8 @@ let cookieClick = document.getElementById("cookie");
 
 let bonus1 = document.getElementById("bonus1"); //OK
 let bonus2 = document.getElementById("bonus2"); //OK
-let bonus3 = document.getElementById("bonus3");
-let bonus4 = document.getElementById("bonus4");
+let bonus3 = document.getElementById("bonus3"); //OK
+let bonus4 = document.getElementById("bonus4"); //OK
 
 let bonus1Multiplicator = 2;
 let bonus2Multiplicator = 3;
@@ -23,13 +23,20 @@ let bonus1Increment = 5;
 bonus1CostElement.innerHTML = bonus1Cost;
 
 let bonus2CostElement = document.getElementById("valeurCoutBonus2");
-let bonus2Cost = 25;
+let bonus2Cost = 100;
+let bonus2Increment = 50;
 bonus2CostElement.innerHTML = bonus2Cost;
 
-// let bonus3Cost = 1000;
-// let bonus4Cost = 10000;
-console.log(bonus1Cost);
-let newBonus1cost = bonus1Cost + (bonus1Cost / 100 * 10);
+let bonus3CostElement = document.getElementById("valeurCoutBonus3");
+let bonus3Cost = 125;
+let bonus3Increment = 500;
+bonus3CostElement.innerHTML = bonus3Cost;
+
+let bonus4CostElement = document.getElementById("valeurCoutBonus4");
+let bonus4Cost = 10000;
+let bonus4Increment = 5000;
+bonus4CostElement.innerHTML = bonus4Cost;
+
 
 
 let bonus1Number = 0;
@@ -52,26 +59,42 @@ function autoClicker() {
 }
 //window.setInterval(auto, 1000);
 
+function startCoundown(){
+  let interval = setInterval(function() {
+    countdown--;
+    document.getElementById("timer").innerText = countdown;
+    applyFunction();
+    if (countdown <=0 ) {
+      clearInterval(interval);
+      totalMultiplicateur = totalMultiplicateur / 2;
+      totalMultiplicateurHtml.innerHTML = totalMultiplicateur;
+      }
+  }, 1000);
+}
+
+
+
 cookieClick.addEventListener("click", clickScore);
 
 
+// cookie.addEventListener("click", () => 
 
-
-cookie.addEventListener("click", () => {
+  function checkScore() {
   totalScoreHtml.innerHTML = totalScore;
 
-  if (totalScore >= 10000){
+  if (totalScore >= bonus4Cost){
     bonus4.disabled = false;
-  } else if (totalScore >= 1000) {
+  } else if (totalScore >= bonus3Cost) {
     bonus3.disabled = false;
   } else if (totalScore >= bonus2Cost) {
     bonus2.disabled = false;
   } else if (totalScore >= bonus1Cost) {
     bonus1.disabled = false;
-  } else {
-    bonus1.disabled = true;
-  }
-});
+  } 
+};
+
+window.setInterval(checkScore, 10);
+
 
 bonus1.addEventListener("click", () => {
   totalMultiplicateur++;
@@ -89,21 +112,21 @@ bonus1.addEventListener("click", () => {
 
 bonus2.addEventListener("click", () =>{
   autoClickNumber++;
-  setInterval(autoClicker, 1000);
+    setInterval(autoClicker, 1000);
   totalScore = totalScore - bonus2Cost;
-  bonus2Cost += 25;
+  bonus2Cost += bonus2Increment;
     bonus2CostElement.innerHTML = bonus2Cost;
+    bonus2Increment +=25;
   if (totalScore < bonus2Cost){
     bonus2.disabled = true;
   }
+})
 
+bonus3.addEventListener("click", () => {
+  countdown = 5;
+  startCoundown();
+  totalMultiplicateur = totalMultiplicateur*2;
+  totalMultiplicateurHtml.innerHTML = totalMultiplicateur;
 })
 
 
-
-
-
-  // totalMultiplicateurHtml = document.getElementById("multiplicateur");
-  //  totalMultiplicateurHtml.innerHTML = totalMultiplicateur;
-  //  totalScore = calculerNewScore(bonus1Cost); 
-  // totalScoreHtml.innerHTML = totalScore;
